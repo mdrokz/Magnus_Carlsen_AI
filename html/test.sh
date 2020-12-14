484ee8fcc4aebf62b982ef933bd58adcaf0aa7f8
mdrokz <mohammadmunshi@gmail.com>
2020-12-15 03:26:04 +0530

    Added json string construction

--- html/test.sh
+++ html/test.sh
@@ -1,5 +1,6 @@
 for i in {1..6}
 do
     #wget --quiet -O - "https://chessgames.com/perl/chess.pl?page=$i&pid=52948" | pup 'a attr{href}' | perl -ne 'print if /^(?!.*com).*(gid=.*)/' >> "chessgame_url.txt" &
-    echo $i
+    echo "{\"link\": \"$i\"}"
 done
+
--- html/write.sh
+++ html/write.sh
@@ -7,26 +7,33 @@ getUrl() {
 
     # [ "$bytes" == "6148" ] || [ "$bytes" == "6147" ]
 
-    if [ "$bytes" -gt "6000" ]
-    then
-        echo $x | pup 'a attr{href}' | perl -ne 'print if /^(?!.*com).*(gid=.*)/' >> "chessgame_url.txt"
+    if [ "$bytes" -gt "6000" ]; then
+        local links=($(echo $x | pup 'a attr{href}' | perl -ne 'print if /^(?!.*com).*(gid=.*)/'))
+        local moves=($(echo $x | pup 'td[align="RIGHT"]:nth-child(4) > font text{}'))
+        # local titles=($(echo $x | pup 'font[face="verdana,arial,helvetica"] > a text{}' | grep vs))
+        moves=("${moves[@]:1}")
+
+        local json_string=""
+
+        for i in ${!moves[@]}; do
+            local move=${moves[$i]}
+            local link=${links[$i]}
+            json_string="$json_string{\"link\": \"$link\",\"move\": \"$move\"},"
+        done
+
+        echo $json_string >>chess_game.json
     else
         getUrl $1 &
     fi
 }
 
-for i in {1..144}
-do
+echo "[" >chess_game.json
+
+for i in {1..144}; do
     #wget --quiet -O - "https://chessgames.com/perl/chess.pl?page=$i&pid=52948" | pup 'a attr{href}' | perl -ne 'print if /^(?!.*com).*(gid=.*)/' >> "chessgame_url.txt" &
     getUrl $i &
 done
 
-
-
-#while read p; do
-#wget --quiet -O - "https://chessgames.com/$p" &
-#    #echo "$p"
-#     wget --quiet -O - "https://chessgames.com/$p" | pup 'a:first-of-type json{}' | jq '.[3].text' >> t1.txt &
-#done <t.txt
-
 wait
+
+echo "]" >>chess_game.json
